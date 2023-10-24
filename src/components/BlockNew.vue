c280571518aa56a27687d3865f5ab029f3f92d9c
Reon Kovalchuk <thesmollestlion@gmail.com>
2023-10-24 14:32:06 +0400

    больше категорий

--- src/components/BlockNew.vue
+++ src/components/BlockNew.vue
@@ -9,22 +9,21 @@
       <v-col v-else sm="6" lg="4" v-for="article in topHeadlines.articles">
         <Card :article-data="article" :key="article.article_id"></Card>
       </v-col>
-
     </v-row>
     <v-row>
       <v-col cols="12">
-        <v-btn @click.prevent="$emit('goToPage', 'category', { category: '' })">Еще</v-btn>
+        <v-btn @click.prevent="$emit('goToPage', 'category', { category: '' })"
+          >Еще</v-btn
+        >
       </v-col>
     </v-row>
   </my-sheet>
 </template>
 
 <script setup>
+import Card from "@/components/Card.vue";
+import { getTopHeadlines } from "@/script/getData";
+import MySheet from "@/components/MySheet.vue";
 
-import Card from '@/components/Card.vue';
-import { getTopHeadlines } from '@/script/getData'
-import MySheet from '@/components/MySheet.vue';
-
-const topHeadlines = await getTopHeadlines('', 8);
-
+const topHeadlines = await getTopHeadlines("", 9);
 </script>
--- src/data/categories.js
+++ src/data/categories.js
@@ -1,23 +1,13 @@
-// export default [
-//   { name: "business", ru: "Бизнес" },
-//   { name: "entertainment", ru: "Развлечения" },
-//   { name: "general", ru: "Общее" },
-//   { name: "health", ru: "Здоровье" },
-//   { name: "science", ru: "Наука" },
-//   { name: "sports", ru: "Спорт" },
-//   { name: "technology", ru: "Технологии" },
-// ]
-
 export default [
   { name: "business", ru: "Бизнес" },
-  // { name: "entertainment", ru: "Развлечения" },
+  { name: "entertainment", ru: "Развлечения" },
   // { name: "environment", ru: "Природа" },
   // { name: "food", ru: "Еда" },
-  // { name: "health", ru: "Здоровье" },
-  // { name: "politics", ru: "Политика" },
-  // { name: "science", ru: "Наука" },
-  // { name: "sports", ru: "Спорт" },
-  // { name: "technology", ru: "Технологии" },
+  { name: "health", ru: "Здоровье" },
+  { name: "politics", ru: "Политика" },
+  { name: "science", ru: "Наука" },
+  { name: "sports", ru: "Спорт" },
+  { name: "technology", ru: "Технологии" },
   // { name: "tourism", ru: "Туризм" },
   // { name: "world", ru: "Мировые" },
-]
+];
